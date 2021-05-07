6e32fae167295e85284411dbd5c83d8c49b7ec1f
Henok Abatihun <33490057+henokabat@users.noreply.github.com>
2021-05-07 15:35:29 -0500

    Initial commit

---
+++ src/main/java/edu/miu/appointmentreservationp/AppointmentReservationProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.appointmentreservationp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentReservationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentReservationProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/miu/appointmentreservationp/AppointmentReservationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.appointmentreservationp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentReservationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
